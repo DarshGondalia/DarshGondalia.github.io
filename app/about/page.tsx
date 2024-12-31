e1f4b3d85c8e67f761eafd38bb456f17e1275c90
Darsh Gondalia <51378290+DarshGondalia@users.noreply.github.com>
2024-12-30 22:45:48 -0500

    Update is Left

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -255,7 +255,7 @@ export default function AboutPage() {
               {/* Experiences under the Year */}
               {yearlyExp.experiences.map((exp, expIndex) => {
                 // Determine alignment based on experience index
-                const isLeft = expIndex % 2 === 0
+                const isLeft = yearIndex % 2 === 0
                 const uniqueId = `${yearlyExp.year}-${expIndex}`
 
                 return (
